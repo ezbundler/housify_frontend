9c794e285f33864ff01229128d440114fd7ade4e
ezbundler <developer.akash1236@gmail.com>
2024-09-29 10:23:00 +0530

    slider in the listing

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import Profile from './pages/Profile.jsx'
 import Header from './components/Header.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import CreateListing from './pages/createListing.jsx'
-import Listings from './pages/Listings.jsx'
+
 import UpdateListing from './pages/UpdateListing.jsx'
+import Listing from './pages/Listing.jsx'
 
 
 const App = () => {
@@ -19,10 +20,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/listing/:listingId" element={<Listings />} />
-
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
+      <Route path='/listing/:listingId' element={<Listing />} />
       <Route  element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing/>}/>
